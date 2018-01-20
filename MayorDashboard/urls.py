36af0c4caab8547d3f96442feb21c128a80203e6
Max <maxime.manote@polytechnique.edu>
2018-01-20 13:32:56 +0800

    Reupdated urls

--- MayorDashboard/urls.py
+++ MayorDashboard/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from rest_framework import routers
 from MayorDashboard.quickstart import views
+from misc import urls as misc_urls
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -24,9 +25,10 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    ]
+    url(r'^api', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(misc_urls)),
+]
 
 
 
